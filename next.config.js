9b8a75c9f3b9cd9db62d1827655410b4f6070cb6
thasup <thanachonfirst@hotmail.com>
2023-06-19 12:12:16 +0700

    Revert "ci(next.config.js): add output option for deployment on GitHub Pages"
    This reverts commit 3fb62695fa7a3762f22e95485e83d138c610571d.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   assetPrefix: isProd ? "/travelmate-web/" : "",
 };
